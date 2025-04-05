daf88d2527d61bf0f47c15eb129e2bb47110e3b0
Fable42 <kovalskii.4@mail.ru>
2025-04-05 22:56:13 +0300

    Add basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,51 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'ffaker'
+
+equipment =[]
+5.times do
+  equipment << Helmet.create!(
+    ventilation_type: %w[Passive Active].sample,
+    has_audio_support: [true, false].sample,
+    certifications: "ASTM #{rand(100..500)}",
+  )
+end
+
+5.times do
+  equipment << Ski.create!(
+    sidecut_radius: rand(10.0..25.0).round(2),
+    waist_width: rand(70..100),
+    binding_type: %w[Fixed Adjustable].sample,
+    is_twin_tip: [true, false].sample,
+    core_type: %w[Wood Carbon Hybrid].sample,
+    recommended_min_weight: rand(40..60),
+    recommended_max_weight: rand(70..100)
+  )
+end
+
+5.times do
+  equipment << Snowboard.create!(
+    shape: %w[Directional Twin Hybrid].sample,
+    flex: %w[Soft Medium Stiff].sample,
+    profile: %w[Camber Rocker Flat].sample,
+    recommended_min_weight: rand(40..60),
+    recommended_max_weight: rand(70..100)
+  )
+end
+
+equipment.each do |e|
+  Equipment.create!(
+    storable_type: e.class.name,
+    storable_id: e.id,
+    size: %w[Small Medium Large].sample,
+    color: FFaker::Color.name,
+    brand: FFaker::Company.name,
+    model: "Model-#{rand(1000..9999)}",
+    article_number: Array.new(10) { rand(36).to_s(36) }.join.upcase,
+    description: FFaker::Lorem.sentence,
+    material: %w[Plastic Metal Wood].sample,
+    weight: rand(1..10),
+    purpose: %w[Skiing Snowboarding].sample
+  )
+end
+
+
+puts "Seeding complete!"
