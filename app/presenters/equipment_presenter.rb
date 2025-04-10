4c88996cb742007525637861cd4bc0e1bdf8eb5c
Fable42 <kovalskii.4@mail.ru>
2025-04-10 20:45:22 +0300

    Add equipment presenter

---
+++ app/presenters/equipment_presenter.rb
@@ -0,0 +1,39 @@
+# typed: strict
+
+class EquipmentPresenter
+  extend T::Sig
+
+  class EquipmentAttributes < T::Struct
+    const :id, Integer
+    const :type, String
+    const :article_number, String
+    const :brand, String
+    const :model, String
+    const :color, String
+    const :size, String
+    const :material, String
+    const :weight, Integer
+    const :purpose, String
+  end
+
+  def equipment_attributes_list(equipment_list)
+    equipment_list.map do |equipment|
+      equipment_attributes(equipment)
+    end
+  end
+
+  def equipment_attributes(equipment, storable: nil)
+    EquipmentAttributes.new(
+      id: equipment.id,
+      type: equipment.storable_type,
+      article_number: equipment.article_number,
+      brand: equipment.brand,
+      model: equipment.model,
+      color: equipment.color,
+      size: equipment.size,
+      material: equipment.material,
+      weight: equipment.weight,
+      purpose: equipment.purpose
+    )
+  end
+end
